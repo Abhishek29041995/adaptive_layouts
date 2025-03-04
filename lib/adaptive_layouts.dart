ecfc995a913528efb6ad310b353190cff9609db9
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-04 09:42:46 +0530

    export file public

--- lib/adaptive_layouts.dart
+++ lib/adaptive_layouts.dart
@@ -4,3 +4,4 @@ export 'src/responsive/responsive_helper.dart';
 export 'src/navigation/adaptive_navigation.dart';
 export 'src/navigation_mode.dart';
 export 'src/scroll_views/adaptive_scroll_view.dart';
+export 'src/scroll_views/group_item.dart';
