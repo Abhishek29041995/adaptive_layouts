0a05a5f160c514fe53477f5c82091f2a339716e0
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-04 14:23:32 +0530

    navigation adaptive modification

--- lib/adaptive_layouts.dart
+++ lib/adaptive_layouts.dart
@@ -5,3 +5,4 @@ export 'src/navigation/adaptive_navigation.dart';
 export 'src/navigation_mode.dart';
 export 'src/scroll_views/adaptive_scroll_view.dart';
 export 'src/scroll_views/group_item.dart';
+export 'src/navigation/adaptive_scaffold.dart';
