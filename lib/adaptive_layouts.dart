4d52a06c2782f8b61a9a6db4fddb3b3d8b7eef70
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-04 16:45:35 +0530

    navigation adaptive modification

--- lib/adaptive_layouts.dart
+++ lib/adaptive_layouts.dart
@@ -7,5 +7,8 @@ export 'src/scroll_views/adaptive_scroll_view.dart';
 export 'src/scroll_views/group_item.dart';
 export 'src/navigation/adaptive_scaffold.dart';
 export 'src/navigation/navigation_item.dart';
-export 'src/navigation/auto_route_helper_impl.dart';
-export 'src/navigation/auto_route_helper.dart';
+
+// Conditional import for AutoRouteHelper
+export 'src/navigation/auto_route_helper.dart'
+    if (dart.library.io) 'src/navigation/auto_route_helper_impl.dart'
+    if (dart.library.html) 'src/navigation/auto_route_helper_fallback.dart';
