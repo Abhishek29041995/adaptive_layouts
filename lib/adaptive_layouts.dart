d5124f8566008f4d9d718d1d66f0c2db2087ecf6
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-06 10:53:29 +0530

    adaptive layout support

--- lib/adaptive_layouts.dart
+++ lib/adaptive_layouts.dart
@@ -3,3 +3,4 @@ library adaptive_layouts;
 export 'src/responsive/responsive_helper.dart';
 export 'src/scroll_views/adaptive_scroll_view.dart';
 export 'src/scroll_views/group_item.dart';
+export 'src/adaptive_scaffold.dart';
