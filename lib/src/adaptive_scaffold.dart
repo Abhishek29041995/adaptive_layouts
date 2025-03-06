9e6917c56d6388033d6ab3c388fc269531440848
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-06 10:48:13 +0530

    adaptive layout support

---
+++ lib/src/adaptive_scaffold.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../src/responsive/responsive_helper.dart';
+
+class AdaptiveScaffold extends StatelessWidget {
+  final PreferredSizeWidget? appBar;
+  final Widget? floatingActionButton;
+  final Widget? drawer;
+  final Widget? endDrawer;
+  final Widget? bottomSheet;
+  final Widget? body;
+  final Widget? bottomNavigationBar;
+  final List<NavigationRailDestination>? navigationRailDestinations;
+  final ValueChanged<int>? onTap;
+  final int currentIndex;
+
+  const AdaptiveScaffold({
+    super.key,
+    this.appBar,
+    this.floatingActionButton,
+    this.drawer,
+    this.endDrawer,
+    this.bottomSheet,
+    required this.body,
+    this.bottomNavigationBar,
+    this.navigationRailDestinations,
+    this.onTap,
+    this.currentIndex = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final breakpoint = Responsive.currentBreakpoint(context);
+    final bool isTablet = breakpoint == Breakpoint.tablet;
+    final bool isDesktop = breakpoint == Breakpoint.desktop ||
+        breakpoint == Breakpoint.web ||
+        breakpoint == Breakpoint.xl ||
+        breakpoint == Breakpoint.xxl;
+
+    return Scaffold(
+      appBar: appBar,
+      drawer: !isDesktop ? drawer : null,
+      endDrawer: !isDesktop ? endDrawer : null,
+      body: Row(
+        children: [
+          if (isDesktop && drawer != null) SizedBox(width: 250, child: drawer),
+          if (isTablet && navigationRailDestinations != null)
+            NavigationRail(
+              selectedIndex: currentIndex,
+              onDestinationSelected: onTap,
+              labelType: NavigationRailLabelType.selected,
+              destinations: navigationRailDestinations!,
+            ),
+          Expanded(child: body!),
+          if (isDesktop && endDrawer != null)
+            SizedBox(width: 250, child: endDrawer),
+        ],
+      ),
+      bottomNavigationBar:
+          (!isDesktop && !isTablet) ? bottomNavigationBar : null,
+      floatingActionButton: floatingActionButton,
+      bottomSheet: bottomSheet,
+    );
+  }
+}
