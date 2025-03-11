e1d5d3aa486a645638e46c3bf4b8983e97cf502f
Abhishek Suman <abhishek.s@nzminds.com>
2025-03-11 11:56:13 +0530

    horizontal list support

--- lib/adaptive_layouts.dart
+++ lib/adaptive_layouts.dart
@@ -4,3 +4,6 @@ export 'src/responsive/responsive_helper.dart';
 export 'src/scroll_views/adaptive_scroll_view.dart';
 export 'src/scroll_views/group_item.dart';
 export 'src/adaptive_scaffold.dart';
+
+export 'src/scroll_views/horizontal_scroll_view.dart';
+export 'src/scroll_views/snap_scroll_physic.dart';
